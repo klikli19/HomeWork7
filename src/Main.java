fe8d1e165fac24144647e391b48a6dfd2e4ad5da
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-08-21 20:44:35 +0300

    сделала основные задачи +2 доп.

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,25 @@ public class Main {
         fullName = "Иванов Семён Семёнович";
         System.out.println("Данные ФИО сотрудника - " + fullName.replace('ё', 'e') + "\n");
 
+        System.out.println("Задание 4");
+        fullName = "Иванов Иван Иванович";
+        int a = fullName.indexOf(' ');
+        int b = fullName.indexOf(' ', a+1);
+        int c = fullName.lastIndexOf(' ');
+        System.out.println("Имя сотрудника - " + fullName.substring(a,b));
+        System.out.println("Отчество сотрудника" + fullName.substring(c));
+        System.out.println("Фамилия сотрудника - " + fullName.substring( 0, a) + "\n");
+
+        System.out.println("Задание 5");
+        fullName = "иванов иван иванович";
+        char [] name = fullName.toCharArray();
+        for (int i = 1; i < name.length; i++){
+            if(name[i - 1] == ' ' &&  name[i] != ' ' && name[0] != ' ')
+                name[i] = Character.toUpperCase(name[i]);
+        }
+        if (name[0] != ' ')
+            name[0] = Character.toUpperCase(name[0]);
+        System.out.println(name);
+
     }
 }
